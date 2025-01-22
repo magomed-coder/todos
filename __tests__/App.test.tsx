4a6e8d5ff203580f3da2296d9ab8ccc3f7817748
Magomed Khadzhiev <m.hadjiev@dhabits.ru>
2025-01-23 00:57:11 +0300

    fix

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import App from "../App";
+import App from "../src/App";
 
 beforeEach(() => {
   localStorage.clear();
